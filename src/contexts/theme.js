9fe7b681fef7ddad51c468865cc4b15b07ff4b66
Vismay <vismay@ceegees.in>
2023-09-07 20:28:25 +0530

    Prefered Theme changed

--- src/contexts/theme.js
+++ src/contexts/theme.js
@@ -7,7 +7,7 @@ const ThemeProvider = ({ children }) => {
   const [themeName, setThemeName] = useState('light')
 
   useEffect(() => {
-    const darkMediaQuery = window.matchMedia('(prefers-color-scheme: dark)');
+    const darkMediaQuery = window.matchMedia('(prefers-color-scheme: light)');
     setThemeName(darkMediaQuery.matches ? 'dark' : 'light')
     darkMediaQuery.addEventListener('change', (e) => {
       setThemeName(e.matches ? 'dark' : 'light')
